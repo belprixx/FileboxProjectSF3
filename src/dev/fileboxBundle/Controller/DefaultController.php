b876b6441d699bda94d3d1a2a97f6af47cb70b68
Theo Tison <tison.theo@gmail.com>
2016-05-12 19:08:51 +0200

    test2

--- src/dev/fileboxBundle/Controller/DefaultController.php
+++ src/dev/fileboxBundle/Controller/DefaultController.php
@@ -4,6 +4,11 @@ namespace dev\fileboxBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use \Dropbox as dbx;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class DefaultController extends Controller
 {
@@ -12,6 +17,25 @@ class DefaultController extends Controller
      */
     public function indexAction()
     {
-        return $this->render('devfileboxBundle:Default:home.html.twig');
+        return $this->render('devfileboxBundle:Default:home.html.twig', array('url' => ''));
+    }
+    /**
+     * @Route("/hello")
+     */
+    public function helloAction()
+    {
+        $cle = NULL;
+        $path = __DIR__."/../Resources/config/configDropbox.json";
+        $appInfo = dbx\AppInfo::loadFromJsonFile($path);
+        $webAuth = new dbx\WebAuthNoRedirect($appInfo, "PHP-Example/1.0");
+        $authorizeUrl = $webAuth->start();
+        //$webAuth->finish($authCode);
+        $form = $this ->createFormBuilder()
+            ->add('cle')
+            ->add('send', SubmitType::class , array("label" => 'OK'))
+            ->getForm();
+        //$webAuth->finish($cle);
+        return $this->render('devfileboxBundle:Default:home.html.twig',array('url' => $authorizeUrl,
+                                                                             'form' => $form->createView()));
     }
 }
